e1a5f7ab7658b558f0ba6d9533a003bd73095108
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-07-25 15:28:54 +0430

    generate_question for loop code.

--- MathQuezeGenerator/qestionGenerator.py
+++ MathQuezeGenerator/qestionGenerator.py
@@ -17,3 +17,28 @@ def generate_question():
     else:
         raise ValueError("Unknown operation")
     
+question_count = 5
+score = 0
+time_limit = 20
+for i in range(question_count):
+    try:
+        start_time = time.time()
+        answer = generate_question()
+        user_answer = float(input("Your answer: "))
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        if elapsed_time < time_limit:
+            if user_answer == answer:
+                score += 1
+                print("Correct!")
+            else:
+                print(f"Wrong! The correct answer is {answer}.")
+        else:
+            print("Time's up! You took too long to answer.")
+        print(f"Your current score is: {score}")
+    
+    except:
+        print("Invalid input. Please enter a number.")
+        continue
+
+  
