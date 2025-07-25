a8818e5ad48895da750cdc54e799d7fc9c3d1396
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-07-25 15:47:16 +0430

    generate_question code all update.

--- MathQuezeGenerator/qestionGenerator.py
+++ MathQuezeGenerator/qestionGenerator.py
@@ -7,39 +7,50 @@ def select_difficulty():
     print("2. Medium")
     print("3. Hard")
     while True:
-        choice = int(input("Enter your choice (1/2/3): "))
-        if choice == 1:
-            return (1,10)
-        elif choice == 2:
-            return (1, 100)
-        elif choice == 3:
-            return (1, 1000)
-        else:
-            print("Invalid choice. Please enter 1, 2 or 3.")
-            
+        try:
+            choice = int(input("Enter your choice (1/2/3): "))
+            if choice == 1:
+                return (1, 10)
+            elif choice == 2:
+                return (1, 100)
+            elif choice == 3:
+                return (1, 1000)
+            else:
+                print("Invalid choice. Please enter 1, 2 or 3.")
+        except:
+            print("Invalid input. Please enter a number.")
+
 def generate_question(min_val, max_val):
     number1 = random.randint(min_val, max_val)
     number2 = random.randint(min_val, max_val)
     operation = random.choice(['+', '-', '*', '/'])
-    print(f"Question: What is {number1} {operation} {number2}?")
-    if operation == '+':
-        return number1 + number2
-    elif operation == '-':
-        return number1 - number2
-    elif operation == '*':
-        return number1 * number2
-    elif operation == '/':
-        return number1 / number2
-    else:
-        raise ValueError("Unknown operation")
     
+    if operation == '/':
+        while number2 == 0:
+            number2 = random.randint(min_val, max_val)
+    
+    print(f"Question: What is {number1} {operation} {number2}?")
+    
+    if operation == '+':
+        return round(number1 + number2, 2)
+    elif operation == '-':
+        return round(number1 - number2, 2)
+    elif operation == '*':
+        return round(number1 * number2, 2)
+    elif operation == '/':
+        return round(number1 / number2, 2)
+
+min_val, max_val = select_difficulty()
+
+
 question_count = 5
 score = 0
 time_limit = 20
+
 for i in range(question_count):
     try:
         start_time = time.time()
-        answer = generate_question()
+        answer = generate_question(min_val, max_val)
         user_answer = float(input("Your answer: "))
         end_time = time.time()
         elapsed_time = end_time - start_time
@@ -52,10 +63,6 @@ for i in range(question_count):
         else:
             print("Time's up! You took too long to answer.")
         print(f"Your current score is: {score}")
-    
     except:
         print("Invalid input. Please enter a number.")
         continue
-
-
-  
