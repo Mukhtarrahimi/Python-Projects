f45a47a72c5db8616e16480298338abbc0454729
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-01 11:12:46 +0430

    bankAccount class created and constructor function added.

---
+++ banck_system/main.py
@@ -0,0 +1,9 @@
+class BankAccount:
+    def __init__(self, owner_name, account_number, balance, acount_type, pin_code, is_acctive):
+        self.owner_name = owner_name
+        self.account_number = account_number
+        self.account_number = account_number
+        self.balance = balance
+        self.account_number = acount_type
+        self.pin_code = pin_code
+        self.is_acctive = is_acctive
