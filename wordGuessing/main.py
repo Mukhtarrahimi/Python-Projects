74c77a60a0a37e6582c356095f383f9b7672d14c
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-01 14:32:14 +0430

    list name for wrodGuess project.

---
+++ wordGuessing/main.py
@@ -0,0 +1,2 @@
+names = ["Rahimi", "Akbari", "Hussaini", "Mohammadi", "Ahmadi", "Mandegar", "Hassani", "Jafari",
+         "Qasimi", "Alizada", "Amiri", "Nazari", "Rezaie", "Hashimi", "Rasuli", "Rahmani"]
