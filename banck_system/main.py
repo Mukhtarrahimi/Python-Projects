3d993a60de8ba1668cef58607b86e3af6ecece85
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-01 11:17:54 +0430

    show_information function code .

--- banck_system/main.py
+++ banck_system/main.py
@@ -1,9 +1,17 @@
 class BankAccount:
-    def __init__(self, owner_name, account_number, balance, acount_type, pin_code, is_acctive):
+    def __init__(self, owner_name, account_number, balance, account_type, __pin_code, is_acctive):
         self.owner_name = owner_name
         self.account_number = account_number
-        self.account_number = account_number
+        self.account_type = account_type
         self.balance = balance
-        self.account_number = acount_type
-        self.pin_code = pin_code
+        self.__pin_code = __pin_code
         self.is_acctive = is_acctive
+    
+    def show_infomation(self):
+        print("Account Owner: ", self.owner_name)
+        print("Account Number: ", self.account_number)
+        print("Account Type: ", self.account_type)
+        print("Account Balance: ", self.balance)
+        print("Account Status: ", self.is_acctive)
+        
+        
