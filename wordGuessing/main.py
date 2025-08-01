d782ce44c7c3847f2770b0ff76fd2d7034503d95
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-01 14:40:02 +0430

    variable for guessWord project.

--- wordGuessing/main.py
+++ wordGuessing/main.py
@@ -1,2 +1,11 @@
-names = ["Rahimi", "Akbari", "Hussaini", "Mohammadi", "Ahmadi", "Mandegar", "Hassani", "Jafari",
-         "Qasimi", "Alizada", "Amiri", "Nazari", "Rezaie", "Hashimi", "Rasuli", "Rahmani"]
+import random
+names = ["Rahimi", "Akbari", "Hussaini", "Mohammadi", "Ahmadi",
+         "Mandegar", "Hassani", "Jafari","Qasimi", "Alizada",
+         "Amiri", "Nazari", "Rezaie", "Hashimi", "Rasuli", "Rahmani"]
+
+select_name = random.choice(names).lower()
+guess_list = [' - ']* len(select_name)
+current_guess = " ".join(select_name)
+guess_count = len(select_name)
+
+
