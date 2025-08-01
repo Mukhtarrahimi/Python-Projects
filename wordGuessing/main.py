95441bafe6b48506c603062b849f8250ad987fee
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-01 17:08:44 +0430

    new feature added.

--- wordGuessing/main.py
+++ wordGuessing/main.py
@@ -8,6 +8,8 @@ select_name = random.choice(names).lower()
 guess_list = ['-' for _ in select_name]
 guess_count = len(select_name)
 guessed_chars = set()
+correct_char_list = []
+discorrect_char_list = []
 
 print("Guess the name!")
 
@@ -16,13 +18,13 @@ while guess_count > 0:
     guess_char = input("Enter a character: ").lower()
 
     if not guess_char.isalpha() or len(guess_char) != 1:
+        correct_char_list.append(guess_char)
         print("Invalid input. Enter one alphabet letter.")
         continue
 
     if guess_char in guessed_chars:
         print("You already guessed that character.")
         continue
-
     guessed_chars.add(guess_char)
 
     if guess_char in select_name:
@@ -35,8 +37,13 @@ while guess_count > 0:
             print("You win! The name was:", select_name.capitalize())
             break
     else:
+        discorrect_char_list.append(guess_char)
         guess_count -= 1
         print(f"Wrong! Remaining guesses: {guess_count}")
 
 else:
     print("You lost! The correct name was:", select_name.capitalize())
+
+print(f'correct list of character you enter: {correct_char_list}')
+print(f'discorrect list of character you enter: {discorrect_char_list}')
+
