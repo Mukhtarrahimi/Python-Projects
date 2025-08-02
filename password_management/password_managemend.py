e627c22411223ce7d3eac0f9923ad232d5e24328
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-02 15:06:35 +0430

    function for add password in password_management project.

--- password_management/password_managemend.py
+++ password_management/password_managemend.py
@@ -1,12 +1,21 @@
+def add_password(user_name, password):
+    with open("passwords.txt", "a") as f:
+        f.write(f'{user_name} - {password}')
+    print('Added')
+
+# running
 while True:
-    user_input = ("Select your option (v: view, a: add, q: quit)")
-    if user_input.lower() == "v":
-        print("your password are as follow:")
-        pass
-    elif user_input.lower() == "a":
-        print("let's add a new user_password")
-    elif user_input.lower() == "q":
+    user_input = input('Select your option (v: view, a: add, q: quit): ')
+    if user_input.lower() == 'v':
+        print('your password are as follow:')
+        
+    elif user_input.lower() == 'a':
+        print('let\'s add a new user_password')
+        user_name = input('Enter name: ')
+        user_pass = input('Enter password: ')
+        add_password(user_name, user_pass)
+    elif user_input.lower() == 'q':
         break
     else:
-        print("invalid input!")
+        print('invalid input!')
         continue
