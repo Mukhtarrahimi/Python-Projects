acdf2f07f0bb6647c1db55215f212aa3b5e47586
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-02 15:21:37 +0430

    function for view passwrod.

--- password_management/password_managemend.py
+++ password_management/password_managemend.py
@@ -1,13 +1,30 @@
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+FILE_PATH = os.path.join(BASE_DIR, 'passwords.txt')
+
 def add_password(user_name, password):
-    with open("passwords.txt", "a") as f:
-        f.write(f'{user_name} - {password}')
+    with open(FILE_PATH, 'a') as f:
+        f.write(f'{user_name} - {password}\n')
     print('Added')
+    
+def view_password():
+    try:
+        with open(FILE_PATH, 'r') as f:
+            for item in f:
+                item = item.strip()
+                user_name, user_pass = item.split('-')
+                print(f'USERNAME: {user_name} - PASSWORD: {user_pass}')
+    except:
+        print('No passwords saved')
+        
 
 # running
 while True:
     user_input = input('Select your option (v: view, a: add, q: quit): ')
     if user_input.lower() == 'v':
         print('your password are as follow:')
+        view_password()
         
     elif user_input.lower() == 'a':
         print('let\'s add a new user_password')
