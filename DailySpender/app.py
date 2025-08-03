6a83c4b3e2f5f9cabab4ae89109f7a8aeee989cc
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-03 10:15:14 +0430

    add item function code.

--- DailySpender/app.py
+++ DailySpender/app.py
@@ -1,8 +1,10 @@
 import sqlite3 as db
+import datetime
 conn = db.connect('database.db')
 c = conn.cursor()
 
 def create_table():
+    # intial database
     c.execute('''CREATE TABLE IS NOT EXISTS expenses (
                  amount REAL,
                  category TEXT COLLATE NOCASE,
@@ -10,5 +12,17 @@ def create_table():
                  date TEXT
                  )
                  ''')
+    conn.commit()
+    conn.close()
+    
+def add(amount, category, message, date):
+    # add to database
+    date = str(datetime.now().strftime('%y - %m - %d | %H:%M'))
+    c.execute("INSERT INTO expenses VALUES (:amount, :category, :message , :date)", {
+        'amount': amount,
+        'category': category,
+        'message': message,
+        'date': date
+    })
     conn.commit()
     conn.close()
