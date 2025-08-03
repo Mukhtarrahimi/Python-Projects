590f31c1cbd7b45f9fbc82ddaf97d2be3ad5a5fb
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-03 10:41:42 +0430

    usage & args code for spend_app.py file.

--- DailySpender/spend_app.py
+++ DailySpender/spend_app.py
@@ -1,3 +1,32 @@
 from docopt import docopt
 from tabulate import tabulate
 from api import *
+
+usage = '''
+usage:
+    spend_app.py --init
+    spend_app.py --show [<category>]
+    spend_app.py --add <category> [<message>]
+    
+'''
+
+args = docopt(usage)
+
+if args ['--init']:
+    create_table()
+    print('Your Table has been initialized')
+if args ['--show']:
+    category = args['<category>']
+    amount, results = show(category)
+    print(f'Total expenses: {amount}')
+    print(tabulate(results, headers=['Date', 'Amount', 'Message'], tablefmt='orgtbl'))
+    
+if args ['--add']:
+    try:
+        amount = float(args['<category>'])
+        add(amount, args['<category>'], args['<message>'])
+        print('Expense added successfully')
+    except :
+        print(usage)
+        
+        
